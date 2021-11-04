9b93687eccd542cf27c97e2520444e28c9779951
André Rossner <rossnerandre@gmail.com>
2021-11-04 17:26:06 -0300

    Add close and show Modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals'
 
 function App() {
+  const [cartIsShown, setCartisShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartisShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartisShown(false);
+  }
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
