2c845b029ef228eef035869564e9e5241fe296cc
André Rossner <rossnerandre@gmail.com>
2021-11-05 10:09:17 -0300

    Add effect cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 import CartIcon from '../Cart/CartIcon'
 
 const HeaderCartButton = (props) => {
+  const [btnisHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const numberOfCartItems = items.reduce(
     (curNumbem, item) => {
       return curNumbem + item.amount;
-    }, 0)
+    }, 0);
+
+  const btnClasses = `${classes.button} ${btnisHighlighted ? classes.bump : ''} `;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items])
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
